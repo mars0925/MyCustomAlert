56ae45a86bac4d83d59cf93cdbc7cd3eb75b6cbf
張宮寶 <mars09250148@gmail.com>
2019-07-04 13:05:02 +0800

    Initial Commit

---
+++ MyCustomAlert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCustomAlert
+//
+//  Created by Mars on 2019/7/4.
+//  Copyright © 2019 drs24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
